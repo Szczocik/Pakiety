80d7ca7ed66d4443901750f8f1eb870c7a3a45e1
popam <popa.mariusz@gmail.com>
2021-09-24 16:19:18 +0200

    otwarcie

--- reader.py
+++ reader.py
@@ -3,15 +3,26 @@ import sys
 import pprint
 
 data = [
-    ['Adam', '1', 'Nowak'],
-    ['Tomek', '3', 'Kowalski'],
-    ['Ada', '4', 'Nowak']
+    ['Adam', '1', 'Kowalski'],
+    ['Marta', '2', 'Wacha'],
+    ['Sebastian', '3', 'Kostka']
 ]
 
-
-input_filename = sys.argv[1:][0]
-output_filename = sys.argv[1:][1]
-changes = sys.argv[1:][2:]
+params = sys.argv[1:]
+print(params)
+input_filename = params[0]
+output_filename = params[1]
+changes = params[2:]
 print(input_filename)
 print(output_filename)
 print(changes)
+
+file = open('data.csv', 'r')
+for line in file.readlines():
+        splitted_line = line.split(';')
+        product_name = splitted_line[0]
+        product_count = splitted_line[1]
+        product_price = splitted_line[2]
+        data = []
+
+file.close()
