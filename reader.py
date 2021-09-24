35e1859899d70932c2b2ee8658684b9e9f7aa566
popam <popa.mariusz@gmail.com>
2021-09-24 15:39:19 +0200

    ustawienie input

--- reader.py
+++ reader.py
@@ -1,1 +1,17 @@
+import os
 import sys
+import pprint
+
+data = [
+    ['Adam', '1', 'Nowak'],
+    ['Tomek', '3', 'Kowalski'],
+    ['Ada', '4', 'Nowak']
+]
+
+
+input_filename = sys.argv[1:][0]
+output_filename = sys.argv[1:][1]
+changes = sys.argv[1:][2:]
+print(input_filename)
+print(output_filename)
+print(changes)
