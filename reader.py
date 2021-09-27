e737475e78cf7142c251d8e1e68ec71db433940e
popam <popa.mariusz@gmail.com>
2021-09-27 17:21:41 +0200

    zapis pliku

--- reader.py
+++ reader.py
@@ -7,36 +7,35 @@ data_output = []
 params = sys.argv[1:]
 
 input_filename = params[0]
-# output_filename = params[1]
+output_filename = params[1]
 # changes = params[2:]
 # print(input_filename)
 # print(output_filename)
 # print(changes)
 # files = files.os.system(ls)
 
-if not input_filename:
-    print('Podana ścieżka jest nieprawidłowa, dostępne pliki w tym katalogu to:')
-else:
-    with open(f'{input_filename}', 'r') as file:
-        for line in file:
-            splitted_line = line.split(';')
-            position_1 = splitted_line[0]
-            position_2 = splitted_line[1]
-            position_3 = splitted_line[2]
-            data = [position_1, position_2, position_3]
 
-            data_output.append(data)
-            print(data)
+with open(f'{input_filename}', 'r') as file:
+    for line in file:
+        splitted_line = line.split(';')
+        position_1 = splitted_line[0]
+        position_2 = splitted_line[1]
+        position_3 = splitted_line[2]
+        data = [position_1, position_2, position_3]
+
+        data_output.append(data)
+        print(data)
 
 
-print(data_output)
+    print(data_output)
 
-# with open('data.csv', 'w') as file:
+with open(f'{input_filename}', 'w') as output_filename:
+    output_filename.write()
 
-# file = open('baza_danych.txt', 'w')
-#         file.write('saldo:' + str(saldo) + '\n')
-#         for product_name, data in store.items():
-#             file.write(str(product_name) + ';' + str(data['count']) + ';' + str(data['price']) + '\n')
-#         file.close()
+file = open('baza_danych.txt', 'w')
+        file.write('saldo:' + str(saldo) + '\n')
+        for product_name, data in store.items():
+            file.write(str(product_name) + ';' + str(data['count']) + ';' + str(data['price']) + '\n')
+        file.close()
 
 
