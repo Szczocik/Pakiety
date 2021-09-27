453ce843c94079b185f6cf3c17e7969a5358c7db
popam <popa.mariusz@gmail.com>
2021-09-27 15:38:53 +0200

    wczytywanie pliku

--- reader.py
+++ reader.py
@@ -2,30 +2,36 @@ import os
 import sys
 import pprint
 
-data = [
-    ['Adam', '1', 'Kowalski'],
-    ['Marta', '2', 'Wacha'],
-    ['Sebastian', '3', 'Kostka']
-]
+data_output = []
 
 params = sys.argv[1:]
-# print(params)
+
 input_filename = params[0]
 # output_filename = params[1]
 # changes = params[2:]
 # print(input_filename)
 # print(output_filename)
 # print(changes)
+# files = files.os.system(ls)
 
-with open('data.csv', 'r') as file:
-    for line in file:
-        print(line)
-    #     splitted_line = line.split(';')
-    #     position_1 = splitted_line[0]
-    #     position_2 = splitted_line[1]
-    #     position_3 = splitted_line[2]
-    #     data = [position_1, position_2, position_3]
+if not input_filename:
+    print('Podana ścieżka jest nieprawidłowa, dostępne pliki w tym katalogu to:')
+else:
+    with open(f'{input_filename}', 'r') as file:
+        for line in file:
+            splitted_line = line.split(';')
+            position_1 = splitted_line[0]
+            position_2 = splitted_line[1]
+            position_3 = splitted_line[2]
+            data = [position_1, position_2, position_3]
 
+            data_output.append(data)
+            print(data)
+
+
+print(data_output)
+
+# with open('data.csv', 'w') as file:
 
 # file = open('baza_danych.txt', 'w')
 #         file.write('saldo:' + str(saldo) + '\n')
