1d7e5746f493283d1296e82b51306591f5164b11
popam <popa.mariusz@gmail.com>
2021-09-28 14:52:34 +0200

    zapis do pliku

--- reader.py
+++ reader.py
@@ -9,9 +9,7 @@ params = sys.argv[1:]
 input_filename = params[0]
 output_filename = params[1]
 changes = params[2:]
-# print(input_filename)
-# print(output_filename)
-# print(changes)
+
 # files = files.os.system(ls)
 
 
@@ -24,24 +22,25 @@ with open(f'{input_filename}', 'r') as file:
         data = [position_1, position_2, position_3]
 
         data_output.append(data)
-        print(data)
+    print(data_output)
 
     for change in changes:
         splitted_change = change.split(',')
         y = splitted_change[0]
         x = splitted_change[1]
         value = splitted_change[2]
-        print(y, x, value)
+    print(y, x, value)
 
 
 
 
-# os.mkdir(f'{output_filename}')
 
 
-# with open(f'{input_filename}', 'w') as output_filename:
-#     data_output.write(f'{position_1}' + ';' + f'{position_2}' + ';' + f'{position_3}')
-#     print(data_output)
+os.mkdir(f'{output_filename}')
+name = f'{output_filename}'
+with open(f'{data_output}', 'w') as output_filename:
+    output_filename.write(f'{position_1}' + ';' + f'{position_2}' + ';' + f'{position_3}' + '\n')
+    print(data_output)
 
 
 
